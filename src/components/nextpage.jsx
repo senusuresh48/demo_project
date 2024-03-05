988c7e80fcca5ab0f8c4d4019a31176aebe5661d
senusuresh48 <senusureh48@gmail.com>
2024-03-05 17:04:45 +0530

    changes

--- src/components/nextpage.jsx
+++ src/components/nextpage.jsx
@@ -1,40 +1,31 @@
-import { useState, useEffect } from "react";
-import Button from '@mui/material/Button';
+import React, { useState, useEffect, useRef } from "react";
 
-function Apiconfig() {
-  const [posts, setPosts] = useState([]);
-  const [showPosts, setShowPosts] = useState(false);
+function Apconfig() {
+  const [photos, setPhotos] = useState([]);
+  const id=useRef(0)
+  const url = "https://jsonplaceholder.typicode.com/photos";
 
-  const url = "https://jsonplaceholder.typicode.com/posts";
-
-  const handleButtonClick = () => {
+  useEffect(() => {
     fetch(url)
       .then(res => {
         if (!res.ok) {
-          throw new Error("Oh no");
-        }
+          return Error("Oh no");
+        } 
         return res.json();
       })
-      .then(data => setPosts(data))
-      .then(() => setShowPosts(true))
-      .catch(error => console.error("Error fetching posts:", error));
-  };
+      .then(data => setPhotos(data));
+  });
 
   return (
     <div className="App">
-      <h1>Posts</h1>
+      <h1>photos</h1>
       <h2>...are here</h2>
-      <Button onClick={handleButtonClick}>Click to view Posts</Button>
 
-      {showPosts && (
-        <div>
-          {posts.map(post => (
-            <div key={post.id}>{post.title}</div>
-          ))}
-        </div>
-      )}
+      {photos.map(photo => (
+        <div key={id.current++}>{photo.title}</div>
+      ))}
     </div>
-  );
+  )
 }
 
-export default Apiconfig;
+export default Apconfig
