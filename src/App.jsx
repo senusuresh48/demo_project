e1e2d2d9ef501f3948b95ae79a5f1513f4189245
Shrinivas-S-S <shrinivas.s@newstreettech.com>
2024-03-15 15:09:35 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,25 @@
-import Form from './components/Form.jsx';
-import './App.css';
-import Muitypography from './components/Muitypography.jsx';
-import Parent from './components/nextpage.jsx';
-import MyApp from './components/contextusage/child.jsx';
-import Login from './pages/Login/Login.jsx';
-import Section from './components/contextusage/Section.jsx';
-import {BrowserRouter as Router , Routes,Route}from 'react-router-dom';
+import Form from "./components/Form.jsx";
+import "./App.css";
+import Muitypography from "./components/Muitypography.jsx";
+import Parent from "./components/nextpage.jsx";
+import MyApp from "./components/contextusage/child.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Section from "./components/contextusage/Section.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-       <Route exact path="/" Component={Form} />
-       <Route path="/nextpage" Component={Muitypography}/>
-       <Route path="/newpage" Component={Parent}/>
-       <Route path='/page' Component={MyApp}/>
-       <Route path='/demo' Component={Login}/>
-       <Route path='/demo1' Component={Section}/>
-
-
-
+        <Route exact path="/demo-project" Component={Form} />
+        <Route path="/nextpage" Component={Muitypography} />
+        <Route path="/newpage" Component={Parent} />
+        <Route path="/page" Component={MyApp} />
+        <Route path="/demo" Component={Login} />
+        <Route path="/demo1" Component={Section} />
       </Routes>
     </Router>
-
   );
 }
 
 export default App;
-
